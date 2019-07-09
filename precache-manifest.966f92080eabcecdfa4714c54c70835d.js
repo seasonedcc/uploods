7867c3196b25eea898c7ded69de939ef9a8d83bf
Gustavo Guichard <gustavoguichard@gmail.com>
2019-07-09 19:26:06 -0300

    Updates

---
+++ precache-manifest.966f92080eabcecdfa4714c54c70835d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c972619e50740c94d551c0e6a4aea2",
+    "url": "/uploods/index.html"
+  },
+  {
+    "revision": "639468c07462ebafe6fa",
+    "url": "/uploods/static/css/main.04f34a25.chunk.css"
+  },
+  {
+    "revision": "20331bb74d0af89af74d",
+    "url": "/uploods/static/js/2.886a6a05.chunk.js"
+  },
+  {
+    "revision": "639468c07462ebafe6fa",
+    "url": "/uploods/static/js/main.aa7fdeda.chunk.js"
+  },
+  {
+    "revision": "c2ba4c5bdebf2871fe1b",
+    "url": "/uploods/static/js/runtime~main.8cbff777.js"
+  }
+]);
