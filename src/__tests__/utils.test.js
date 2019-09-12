1c0fae00fed9472609ace15d76e10e7145426afe
Giovani <sousa_giovani@hotmail.com>
2019-09-12 13:44:51 -0300

    Test processFile

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,4 +1,4 @@
-import { humanizeBytes, getFileData } from '../utils'
+import { humanizeBytes, getFileData, processFile } from '../utils'
 
 jest.mock('@seasonedsoftware/utils', () => ({
   fileToBase64: async () => 'foobar',
@@ -20,5 +20,20 @@ describe('getFileData', () => {
       size: 6,
       parsed: 'foobar',
     })
+  })
+})
+
+describe('processFile', () => {
+  const file = new File(['foobar'], 'filename', { type: 'text/html' })
+  it('returns correctly', async () => {
+    expect(await processFile(file, { overwrite: true })).toEqual(
+      expect.objectContaining({
+        name: 'filename',
+        type: 'text/html',
+        size: 6,
+        parsed: 'foobar',
+        id: 'uploods/filename',
+      }),
+    )
   })
 })
